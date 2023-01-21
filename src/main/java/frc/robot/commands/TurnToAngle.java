718846f65f4c914e5a0f3cc25d35bcbac2355b46
tim.helland <tim.helland@lebanon.k12.or.us>
2023-01-21 12:28:36 -0800

    Example of setting up a PIDCommand.

--- src/main/java/frc/robot/commands/TurnToAngle.java
+++ src/main/java/frc/robot/commands/TurnToAngle.java
@@ -1,25 +1,39 @@
 package frc.robot.commands;
+import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import java.util.function.DoubleConsumer;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-public class TurnToAngle extends CommandBase {
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class TurnToAngle extends PIDCommand {
+
+    public TurnToAngle(double target) {
+      super(new PIDController(0.01, 0.0001, 0.0), DrivetrainSubsystem.getInstance()::getYaw, target, output -> useOutput(output), DrivetrainSubsystem.getInstance());
+      getController().enableContinuousInput(-180, 180);
+      getController().setTolerance(1.0, 0.05);
+    }
     
     @Override
     public void initialize() {}
-  
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
-    }
-  
+   
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {}
+
+   
+    public static void useOutput(double value) {
+
+    }
   
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-      return false;
+      return getController().atSetpoint();
     }    
 }

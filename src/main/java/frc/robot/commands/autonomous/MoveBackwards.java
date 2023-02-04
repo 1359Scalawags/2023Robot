c5e8380361a245b91f98d1c91c5e867986f7bc3d
CedAnimator <Ced601ced@gmail.com>
2023-02-04 10:28:14 -0800

    Added basic cardinal directions

---
+++ src/main/java/frc/robot/commands/autonomous/MoveBackwards.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class MoveBackwards extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  //TODO: Just a test
+  //private final ExampleSubsystem m_subsystem;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  private DrivetrainSubsystem m_drivetrainSubsystem;
+
+  public MoveBackwards(DrivetrainSubsystem driveSystem){
+    m_drivetrainSubsystem = driveSystem;
+    addRequirements(driveSystem);
+  }
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {   
+    m_drivetrainSubsystem.drive(
+      new ChassisSpeeds(-1,0,0)
+    );
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

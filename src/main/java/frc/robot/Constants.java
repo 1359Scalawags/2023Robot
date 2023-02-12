b81e1d30e59db1bc65380bc440b38c6c3e539a5e
CedAnimator <Ced601ced@gmail.com>
2023-02-12 14:40:00 -0800

    AutonomousVchanged 0

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -118,7 +118,7 @@ public final class Constants {
         }
     }
     public static final class Autonomous {
-        public static final double kP = 0;
+        public static final double kP = 0.1;
         public static final double kI = 0;
         public static final double kD = 0;
 

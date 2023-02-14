cdd13d49c64c99f136af9d859f8a856095937311
CedAnimator <Ced601ced@gmail.com>
2023-02-13 19:43:33 -0800

    attempted to add feed forward

--- src/main/java/frc/robot/commands/PlatformBalance.java
+++ src/main/java/frc/robot/commands/PlatformBalance.java
@@ -6,7 +6,9 @@ package frc.robot.commands;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,6 +21,7 @@ public class PlatformBalance extends CommandBase {
   private DrivetrainSubsystem m_drivetrainSubsystem;
 
   private static final PIDController pid = new PIDController(Constants.Autonomous.kP, Constants.Autonomous.kI, Constants.Autonomous.kD);
+  private static final ArmFeedforward feedforward = new ArmFeedforward(Constants.Autonomous.kS, Constants.Autonomous.kV, Constants.Autonomous.kA);
 
 
   /** Creates a new ExampleSubsystem. */
@@ -32,6 +35,7 @@ public class PlatformBalance extends CommandBase {
   public void execute() {
     // This method will be called once per scheduler run 
       double K = pid.calculate(m_drivetrainSubsystem.getPitch());
+      K = K + feedforward.calculate(m_drivetrainSubsystem.getPitch(), 1);
       m_drivetrainSubsystem.drive(
         new ChassisSpeeds(0,K,0)
       );

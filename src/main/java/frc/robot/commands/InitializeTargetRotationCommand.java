a8b707bc41bac5927c87abdf03cfb899d2aa21c1
tim.helland <tim.helland@lebanon.k12.or.us>
2023-02-18 16:06:50 -0800

    Fix initializer command.

--- src/main/java/frc/robot/commands/InitializeTargetRotationCommand.java
+++ src/main/java/frc/robot/commands/InitializeTargetRotationCommand.java
@@ -31,8 +31,8 @@ public class InitializeTargetRotationCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    subsystem.changeRelativeSetPoint("elbow", 0);
-    subsystem.changeRelativeSetPoint("shoulder", 0);
+    subsystem.setElbowSetpoint(subsystem.getElbowPosition());
+    subsystem.setShoulderSetpoint(subsystem.getShoulderPosition());
   }
 
   // Called once the command ends or is interrupted.

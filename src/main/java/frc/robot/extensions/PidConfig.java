ebbc5765dfda5a0558385db0a20d8f99a5d10210
Mr Helland <tim.helland@lebanon.k12.or.us>
2023-02-21 19:11:19 -0800

    Add scaling capability for dashboard entry.

--- src/main/java/frc/robot/extensions/PidConfig.java
+++ src/main/java/frc/robot/extensions/PidConfig.java
@@ -11,10 +11,11 @@ public class PidConfig implements Sendable {
     private double minP, maxP;
     private double minI, maxI;
     private double minD, maxD;
+    private double tuningScale;
 
     private static double defaultMax = 0.0001;
 
-    public PidConfig(double p, double i, double d) {
+    public PidConfig(double p, double i, double d, double tuningScale) {
         this.p = p;
         this.i = i;
         this.d = d;
@@ -24,6 +25,7 @@ public class PidConfig implements Sendable {
         this.maxI = defaultMax;
         this.minD = 0;
         this.maxD = defaultMax;
+        this.tuningScale = tuningScale;
     }
 
     public double getP() {
@@ -75,35 +77,68 @@ public class PidConfig implements Sendable {
     }
 
 
+
     private void safeState() {
         p = 0;
         i = 0;
         d = 0;
     }
 
-    private void setRangeP(double[] range) {
-        this.minP = range[0];
-        this.maxP = range[1];
+    private double getTuningScale() {
+        return tuningScale;
     }
 
-    private void setRangeI(double[] range) {
-        this.minI = range[0];
-        this.maxI = range[1];
+    private void setTuningScale(double value) {
+        tuningScale = value;
     }
 
-    private void setRangeD(double[] range) {
-        this.minD = range[0];
-        this.maxD = range[1];
+    private double getScaledP() {
+        return p / tuningScale;
     }
 
-    private double[] getRangeP() {
-        return new double[] {minP, maxP};
+    private void setScaledP(double value) {
+        p = value * tuningScale;
     }
-    private double[] getRangeI() {
-        return new double[] {minI, maxI};
+
+    private double getScaledI() {
+        return i / tuningScale;
     }
-    private double[] getRangeD() {
-        return new double[] {minD, maxD};
+
+    private void setScaledI(double value) {
+        i = value * tuningScale;
+    }
+
+    private double getScaledD() {
+        return i / tuningScale;
+    }
+
+    private void setScaledD(double value) {
+        i = value * tuningScale;
+    }
+
+    private void setScaledRangeP(double[] range) {
+        this.minP = range[0] * tuningScale;
+        this.maxP = range[1] * tuningScale;
+    }
+
+    private void setScaledRangeI(double[] range) {
+        this.minI = range[0] * tuningScale;
+        this.maxI = range[1] * tuningScale;
+    }
+
+    private void setScaledRangeD(double[] range) {
+        this.minD = range[0] * tuningScale;
+        this.maxD = range[1] * tuningScale;
+    }
+
+    private double[] getScaledRangeP() {
+        return new double[] {minP / tuningScale, maxP / tuningScale};
+    }
+    private double[] getScaledRangeI() {
+        return new double[] {minI / tuningScale, maxI / tuningScale};
+    }
+    private double[] getScaledRangeD() {
+        return new double[] {minD / tuningScale, maxD / tuningScale};
     }    
 
 
@@ -111,12 +146,12 @@ public class PidConfig implements Sendable {
     public void initSendable(SendableBuilder builder) {
         builder.setActuator(true);
         builder.setSafeState(this::safeState);
-        builder.addDoubleProperty("P", this::getP, this::setP);
-        builder.addDoubleProperty("i", this::getI, this::setI);
-        builder.addDoubleProperty("d", this::getD, this::setD);
-        builder.addDoubleArrayProperty("P Range", this::getRangeP, this::setRangeP);
-        builder.addDoubleArrayProperty("I Range", this::getRangeI, this::setRangeI);        
-        builder.addDoubleArrayProperty("D Range", this::getRangeD, this::setRangeD);
+        builder.addDoubleProperty("P", this::getScaledP, this::setScaledP);
+        builder.addDoubleProperty("i", this::getScaledI, this::setScaledI);
+        builder.addDoubleProperty("d", this::getScaledD, this::setScaledD);
+        builder.addDoubleArrayProperty("P Range", this::getScaledRangeP, this::setScaledRangeP);
+        builder.addDoubleArrayProperty("I Range", this::getScaledRangeI, this::setScaledRangeI);        
+        builder.addDoubleArrayProperty("D Range", this::getScaledRangeD, this::setScaledRangeD);
         
     }
 

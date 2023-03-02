9abc076cf01813770f3b6927ce164fe96a3b85fa
kavi <kavicoronel@gmail.com>
2023-03-01 19:32:04 -0800

    made test auto movement command

--- src/main/java/frc/robot/commands/autonomous/TestAutoMovment.java
+++ src/main/java/frc/robot/commands/autonomous/TestAutoMovment.java
@@ -1,13 +1,17 @@
 package frc.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.SetDriveMode;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.DrivetrainSubsystem.DriveModes;
 
 public class TestAutoMovment extends SequentialCommandGroup
 {
-    public TestAutoMovment(DrivetrainSubsystem m_DrivetrainSubsystem)
+    public TestAutoMovment(DrivetrainSubsystem m_DrivetrainSubsystem, boolean includeChargeStation)
     {
         addCommands(
+            new SetDriveMode(m_DrivetrainSubsystem, DriveModes.RobotCentric),
+
             new MoveAhead(m_DrivetrainSubsystem,1,0.5),
 
             new MoveBackwards(m_DrivetrainSubsystem,1,0.5),
@@ -15,5 +19,9 @@ public class TestAutoMovment extends SequentialCommandGroup
             new MoveLeft(m_DrivetrainSubsystem, 1, 0.5),
 
             new MoveRight(m_DrivetrainSubsystem, 1, 0.5));
+
+        if(includeChargeStation) {
+            addCommands(null);
+        }
     }
 }

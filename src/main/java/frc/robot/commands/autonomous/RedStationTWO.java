059d75dcba5bbea003125888e54a5e1ef3d05444
kavi <kavicoronel@gmail.com>
2023-03-01 23:19:40 -0800

    Made 3 starting positions for Red alliance

---
+++ src/main/java/frc/robot/commands/autonomous/RedStationTWO.java
@@ -0,0 +1,31 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.SetDriveMode;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.DrivetrainSubsystem.DriveModes;
+
+//TODO: messure feild and put in distances.
+
+public class RedStationTWO extends SequentialCommandGroup
+{
+    public RedStationTWO(DrivetrainSubsystem m_DrivetrainSubsystem, boolean includeChargeStation)
+    {
+        addCommands(
+            new SetDriveMode(m_DrivetrainSubsystem, DriveModes.RobotCentric),
+// We are moving backwards because the robot will be facing the drivers, not the other side.
+            new MoveBackwards(m_DrivetrainSubsystem,1,0.5));
+
+        if(includeChargeStation) {
+            addCommands(
+                new SetDriveMode(m_DrivetrainSubsystem, DriveModes.RobotCentric),
+            
+                new MoveBackwards(m_DrivetrainSubsystem,1,0.5),
+
+                new MoveLeft(m_DrivetrainSubsystem, 1, 0.5),
+    
+                new MoveForward(m_DrivetrainSubsystem, 1, 0.5)
+            );
+        }
+    }
+}

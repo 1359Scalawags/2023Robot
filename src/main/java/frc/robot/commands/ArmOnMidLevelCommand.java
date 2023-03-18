a4fb05b5de88fef9d9ab0131039ed2d24e2c5798
kavi <kavicoronel@gmail.com>
2023-03-17 19:51:55 -0700

    got rid of slew rate limiter

--- src/main/java/frc/robot/commands/ArmOnMidLevelCommand.java
+++ src/main/java/frc/robot/commands/ArmOnMidLevelCommand.java
@@ -14,8 +14,8 @@ public class ArmOnMidLevelCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
 
   private final ArmSubsystem m_subsystem;
-  private final SlewRateLimiter e_Limiter;
-  private final SlewRateLimiter s_Limiter;
+  // private final SlewRateLimiter e_Limiter;
+  // private final SlewRateLimiter s_Limiter;
   /**
    * Creates a new ExampleCommand.
    *
@@ -23,8 +23,8 @@ public class ArmOnMidLevelCommand extends CommandBase {
    */
   public ArmOnMidLevelCommand(ArmSubsystem subsystem) {
     m_subsystem = subsystem;
-    e_Limiter = new SlewRateLimiter(Constants.Arm.Elbow.slewRateLimiter * 2, -Constants.Arm.Elbow.slewRateLimiter, m_subsystem.getElbowDegree());
-    s_Limiter = new SlewRateLimiter(Constants.Arm.Shoulder.slewRateLimiter  * 2, -Constants.Arm.Shoulder.slewRateLimiter, m_subsystem.getShoulderDegree());
+    // e_Limiter = new SlewRateLimiter(Constants.Arm.Elbow.slewRateLimiter * 2, -Constants.Arm.Elbow.slewRateLimiter, m_subsystem.getElbowDegree());
+    // s_Limiter = new SlewRateLimiter(Constants.Arm.Shoulder.slewRateLimiter  * 2, -Constants.Arm.Shoulder.slewRateLimiter, m_subsystem.getShoulderDegree());
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
@@ -32,16 +32,18 @@ public class ArmOnMidLevelCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    e_Limiter.reset(m_subsystem.getElbowDegree());
-    s_Limiter.reset(m_subsystem.getShoulderDegree());
+    // e_Limiter.reset(m_subsystem.getElbowDegree());
+    // s_Limiter.reset(m_subsystem.getShoulderDegree());
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     //TODO: What is optimal angles for parking?
-    m_subsystem.setElbowSetpoint(e_Limiter.calculate(Constants.Arm.Elbow.onMidLevel));
-    m_subsystem.setShoulderSetpoint(s_Limiter.calculate(Constants.Arm.Shoulder.onMidLevel));
+    // m_subsystem.setElbowSetpoint(e_Limiter.calculate(Constants.Arm.Elbow.onMidLevel));
+    // m_subsystem.setShoulderSetpoint(s_Limiter.calculate(Constants.Arm.Shoulder.onMidLevel));
+    m_subsystem.setElbowSetpoint(Constants.Arm.Elbow.onMidLevel);
+    m_subsystem.setShoulderSetpoint(Constants.Arm.Shoulder.onMidLevel);
   }
 
   // Called once the command ends sor is interrupted.

16d4bdef5c9735406425b9d84650bf6be26ade7c
Mr. Helland <tim.helland@lebanon.k12.or.us>
2023-03-19 15:47:56 -0700

    Framework for a pipeline command.

---
+++ src/main/java/frc/robot/commands/autonomous/ChoosePipeLineByLightAndTarget.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import frc.robot.subsystems.VisionSystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/** An example command that uses an example subsystem. */
+public class ChoosePipeLineByLightAndTarget extends CommandBase {
+
+  private final VisionSystem m_subsystem;
+  private final Lighting m_lighting;
+  private final Targets m_target;
+
+  public enum Lighting {
+    White,
+    Yellow
+  }
+
+  public enum Targets {
+    Cone,
+    Cube,
+    Pole
+  }
+
+
+  /** 
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public ChoosePipeLineByLightAndTarget(VisionSystem subsystem, Lighting light, Target target) {
+    m_subsystem = subsystem;
+    m_lighting = light;
+    m_target = target;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
+

dc2b522623913742a50e1445cfeb57dd8c1f697d
Mr Helland <tim.helland@lebanon.k12.or.us>
2023-03-19 10:21:36 -0700

    Remove unnecessary helper class.

--- src/main/java/frc/robot/extensions/ArmGroundLimiter.java
+++ src/main/java/frc/robot/extensions/ArmGroundLimiter.java
@@ -2,19 +2,6 @@ package frc.robot.extensions;
 
 public class ArmGroundLimiter {
 
-    public class Point2D {
-        public double x;
-        public double y;
-        public Point2D() {
-            this.x = 0;
-            this.y = 0;
-        }
-        public Point2D(double x, double y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     public class ArmSegment {
         private double length;
         private double floorAngleOffset;

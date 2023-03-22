f510b5cdfab492cad03f2a902b3bf7fd151bfd9a
WildstrikePlayz <jasmer1702@lebanon.k12.or.us>
2023-03-21 18:31:22 -0700

    Raised trigger zone of arm function

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -109,7 +109,7 @@ public final class Constants {
 
     public static final class Arm {
 
-        public static final double triggerZone = 10;
+        public static final double triggerZone = 15;
 
         public static final double parkingTolerance = 3.0;
         //public static final double radtoDegreeDivisor = Math.PI / 180.0; // name did not match conversion

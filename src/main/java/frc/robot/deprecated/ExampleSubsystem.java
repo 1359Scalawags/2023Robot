49a3ef73f92eafa3e20df04269ce0cebdfd862b9
mrhelland <tim.helland@lebanon.k12.or.us>
2023-03-23 10:03:14 -0700

    Move deprecated files.

--- src/main/java/frc/robot/deprecated/ExampleSubsystem.java
+++ src/main/java/frc/robot/deprecated/ExampleSubsystem.java
@@ -2,14 +2,14 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.deprecated;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class NavigationSubsystem extends SubsystemBase {
+public class ExampleSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public NavigationSubsystem() {}
+  public ExampleSubsystem() {}
 
   /**
    * Example command factory method.
